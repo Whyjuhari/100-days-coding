2a188363551a5eaea5391e41d25956986a53ad39
Juhari <91404024+Whyjuhari@users.noreply.github.com>
2023-10-16 11:47:59 -0400

    days_8_arrays_multi_dimensi

---
+++ DAYS_8/Latihan.java
@@ -0,0 +1,35 @@
+package arrays.DAYS_8;
+
+
+import java.util.Scanner;
+public class Latihan {
+
+    public static void main(String[] args) {
+        
+        // Latihan array multi dimensi
+        // membuat array
+        
+        String[][] meja = new String[2][3];
+        try (Scanner inputArray = new Scanner(System.in)) {            
+            // mengisi setiap array
+            for (int bar = 0; bar < meja.length; bar++) {
+                for (int kol = 0; kol < meja[bar].length; kol++) {
+                    System.out.format("Siapa yang akan duduk dimeja (%d, %d): ", bar, kol);
+                    meja[bar][kol] = inputArray.nextLine();
+                }
+            }
+    
+            // menampilkan array
+            System.out.println("-----------------------------");
+            for (int bar = 0; bar < meja.length; bar++) {
+                for (int kol = 0; kol < meja[bar].length; kol++) {
+                    System.out.format("| %s | \t", meja[bar][kol]);
+                }
+                System.out.println("");
+            }
+            System.out.println("-----------------------------");
+        }
+
+    }
+    
+}
