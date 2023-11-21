cd84d72eac67681cfb10c9689a5e9ecea038c7de
Juhari <91404024+Whyjuhari@users.noreply.github.com>
2023-11-20 21:04:23 -0500

    Add files via upload

---
+++ DAYS_44/Main.java
@@ -0,0 +1,37 @@
+package DAYS_44;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        String[][] mahasiswa = {
+            {"Nama", "Umur", "Nim", "Jurusan"},
+            {"Juhari", "20", "D0221322", "Informatika"},
+            {"Ar", "15", "D0223122", "Sistem Informasi"},
+            {"Juhar", "18", "D0222231", "Matematika"}
+        };
+        gambarTabel(mahasiswa);
+    }
+
+    public static void gambarTabel(String[][] mahasiswa) {
+        
+        // Menggambar baris header
+        for (int i = 0; i < mahasiswa[0].length; i++) {
+            System.out.print("+------------------");
+        }
+        System.out.println("+");
+
+        // Menggambar data mahasiswa
+        for (String[] baris : mahasiswa) {
+            for (String sel : baris) {
+                System.out.printf("| %-17s", sel);
+            }
+            System.out.println("|");
+
+            // Membuat garis horizontal antara setiap baris
+            for (int i = 0; i < baris.length; i++) {
+                System.out.print("+------------------");
+            }
+            System.out.println("+");
+        }
+    }
+}
