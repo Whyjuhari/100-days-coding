754210256776b4a4a742d785af09cf7ec980c286
Juhari <91404024+Whyjuhari@users.noreply.github.com>
2023-12-29 11:28:12 -0500

    Fungsi KeyBinding dengan Event AbstractAction

---
+++ DAYS_82/App.java
@@ -0,0 +1,18 @@
+package DAYS_82;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+        // Key Bindings = bind a Action to a KeyStore
+        //                don't require you to click a component to give it focus
+        //                all Swing components use Key Bindings
+        //                increased flexibility compared to KeyListeners
+        //                can assign key strokes to individual Swing components
+        //                more difficult to utilize and set up :(
+
+
+        Game game = new Game();
+        
+    }
+}
