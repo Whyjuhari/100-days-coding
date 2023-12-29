d486afffbd1b5b0f7cd712d4fee20921440058fa
Juhari <91404024+Whyjuhari@users.noreply.github.com>
2023-12-29 11:29:30 -0500

    Membuat KeyBinding dengan event AbstractionAction

--- DAYS_82/App.java
+++ DAYS_82/App.java
@@ -4,13 +4,12 @@ public class App {
     
     public static void main(String[] args) {
         
-        // Key Bindings = bind a Action to a KeyStore
-        //                don't require you to click a component to give it focus
-        //                all Swing components use Key Bindings
-        //                increased flexibility compared to KeyListeners
-        //                can assign key strokes to individual Swing components
-        //                more difficult to utilize and set up :(
-
+        // Key Binding   = Bind A Tindakan ke Keystore
+        //                 tidak mengharuskan Anda mengklik komponen untuk memberikannya fokus
+        //                 Semua komponen ayunan menggunakan binding kunci
+        //                 Peningkatan fleksibilitas dibandingkan dengan keylisteners
+        //                 dapat menetapkan sapuan kunci ke komponen ayunan individual
+        //                 lebih sulit untuk memanfaatkan dan mengatur :(
 
         Game game = new Game();
         
